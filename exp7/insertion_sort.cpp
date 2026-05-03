eab77a868d7b882c12f640069c5ec58641adc4f8
aaryagaikwad1902-art <aaryagaikwad1902@gmail.com>
2026-05-03 10:04:40 +0530

    Create insertion_sort.cpp

---
+++ exp7/insertion_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];  
+            j--;
+        }
+        arr[j + 1] = key; 
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 1, 9, 3, 7, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, size);
+
+    cout << "Sorted: ";
+    for (int i = 0; i < size; i++) cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
